60aea2c4db36d5f567d5e32c050b408fe54373f2
David Terei <code@davidterei.com>
2016-06-10 13:18:02 -0700

    Improve docs and skeleton support of non x64

--- tsc_unsupported.go
+++ tsc_unsupported.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 //
-// +build amd64
+// +build !amd64
 
 // Package gotsc provides access to the timestamp cycle counter on x86-64 for
 // performing close to cycle accurate benchmarking on x86-64. All recent (think
@@ -15,29 +15,22 @@ package gotsc
 
 // BenchStart obtains the cycle counter. It should be used at the start of
 // benchmarking some code.
-func BenchStart() uint64
+func BenchStart() uint64 {
+  return 0
+}
 
 // BenchEnd obtains the cycle counter. It should be used at the end of
 // benchmarking some code. There is a subtle difference in the allowed
 // reordering of operations between BenchEnd and BenchStart, hence the two
 // functions rather than one.
-func BenchEnd() uint64
+func BenchEnd() uint64 {
+  return 0
+}
 
 // TSCOverhead measures the cycle overhead of calling the underlying `rdtsc`
 // instruction to obtain the current CPU cycle count. You should subtract this
 // value from all your cycle count measurements to accurately benchmark some
 // code.
 func TSCOverhead() uint64 {
-	var t0, t1 uint64
-	overhead := uint64(1000000000000000000)
-
-	for i := 0; i < 100000; i++ {
-		t0 = BenchStart()
-		t1 = BenchEnd()
-		if t1-t0 < overhead {
-			overhead = t1 - t0
-		}
-	}
-
-	return overhead
+  return 0
 }
